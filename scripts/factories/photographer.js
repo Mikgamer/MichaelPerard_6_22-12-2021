d4c0e88f2419dff32d7e212ecbb5e009ceefa59c
Mikgamer <50680361+Mikgamer@users.noreply.github.com>
2021-12-23 16:50:03 +0100

    Modification index.html + scripts
    Modification :
    - index.html
    - script factories/photographer.js
    - css photographer.css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,43 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.className = 'thumb-photographer';
+
+        const user = document.createElement( 'a' );
+        user.href = `photographer-${id}`;
+        user.className = 'user';
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `Image du photographe ${name}`);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const location = document.createElement( 'p' );
+        location.textContent = `${city}, ${country}`;
+        location.className = 'location';
+
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+        tag.className = 'tag';
+
+        const dailyPrice = document.createElement( 'p' );
+        dailyPrice.textContent = `${price}€/jour`;
+        dailyPrice.className = 'dailyPrice';
+
+        user.appendChild(img);
+        user.appendChild(h2);
+        article.appendChild(user);
+
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(dailyPrice);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,24 +1,24 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
         const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
+        {
+			"name": "Tracy Galindo",
+			"id": 82,
+			"city": "Montreal",
+			"country": "Canada",
+			"tagline": "Photographe freelance",
+			"price": 500,
+			"portrait": "account.png"
+		},
+		{
+			"name": "Nabeel Bradford",
+			"id": 527,
+			"city": "Mexico City",
+			"country": "Mexico",
+			"tagline": "Toujours aller de l'avant",
+			"price": 350,
+			"portrait": "account.png"
+		}
         ]
         // et bien retourner le tableau photographers seulement une fois
         return ({
